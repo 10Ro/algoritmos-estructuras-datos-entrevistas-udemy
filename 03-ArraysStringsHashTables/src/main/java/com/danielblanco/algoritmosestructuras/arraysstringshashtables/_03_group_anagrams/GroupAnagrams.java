d9a5f11567babfe47fd9395066e9a72fd5a55e9b
rodrigo <rorofer100@gmail.com>
2025-07-04 12:04:17 +0200

    Ej 03.03 GroupAnagrams

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagrams.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagrams.java
@@ -1,6 +1,12 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._03_group_anagrams;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  * Un anagrama es una palabra creada a partir de la reordenación de las letras de otra palabra. Ej: saco - caso
@@ -13,6 +19,33 @@ import java.util.List;
 public class GroupAnagrams {
 
   public List<List<String>> groupAnagrams(String[] words) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    if (words == null || words.length == 0)
+      return Collections.emptyList();
+
+    Map<String, List<String>> anagramMap = buildAnagramMap(words);
+    return new ArrayList<>(anagramMap.values());
+  }
+
+  private Map<String, List<String>> buildAnagramMap(String[] words) {
+    Map<String, List<String>> map = new HashMap<>();
+    for (String word : words) {
+      String hash = getAnagramHash(word);
+      if (!map.containsKey(hash)) {
+        map.put(hash, new ArrayList<>());
+      }
+
+      map.get(hash).add(word);
+    }
+
+    return map;
+  }
+
+  private String getAnagramHash(String s) {
+    int[] letterCount = new int[26];
+    for (int c : s.toCharArray()) {
+      letterCount[c - 'a']++;
+    }
+
+    return Arrays.toString(letterCount);
   }
 }
